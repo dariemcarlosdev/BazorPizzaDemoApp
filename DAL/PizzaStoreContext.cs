5831fa1684d0b94b71b1ab1203d425e607a2356e
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-05-11 09:28:02 -0400

    Add entity framework support for orders and pizzas

--- DAL/PizzaStoreContext.cs
+++ DAL/PizzaStoreContext.cs
@@ -1,4 +1,6 @@
 using BlazingPizzaNavigation.Model;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazingPizzaNavigation.DAL;
@@ -10,4 +12,24 @@ public class PizzaStoreContext : DbContext
     }
 
     public DbSet<PizzaSpecial> Specials { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Pizza> Pizzas {get; set; }
+    public DbSet<Topping> Toppings {get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Configuring a many-to-many special -> topping relationship that is friendly for serialization
+        //configuring the relationships between your Pizza, Topping, and PizzaTopping entities in Entity Framework Core.
+
+
+        //This line is configuring a composite key for the PizzaTopping entity using PizzaId and ToppingId.
+        modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+        //This line is setting up a one-to - many relationship between Pizza and PizzaTopping.
+        //It’s saying that one Pizza can have many PizzaToppings.
+        modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+        //This line is setting up a many-to -one relationship between PizzaTopping and Topping.
+        //It’s saying that many PizzaToppings can be associated with one Topping.
+        modelBuilder.Entity<PizzaTopping>().HasOne(ps => ps.Topping).WithMany();
+    }
 }
