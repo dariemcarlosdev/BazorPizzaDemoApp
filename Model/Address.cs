6db25ddaaeef597f2604209ef4ca4d2e7b75c863
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-05-14 13:59:59 -0400

    Adding DataAnnotationsValidator for validation

--- Model/Address.cs
+++ Model/Address.cs
@@ -1,11 +1,14 @@
-﻿namespace BlazingPizzaNavigation.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingPizzaNavigation.Model;
+
 
 public class Address
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "You must set a name.")]
     public string Name { get; set; }
-
+    [Required(ErrorMessage = "You must set your address.")]
     public string Line1 { get; set; }
 
     public string Line2 { get; set; }
