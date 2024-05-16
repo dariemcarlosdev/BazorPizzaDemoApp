5cae35b4e949f7cfdd3cf639990ee749a8acd2a6
DARIEM MACIAS MORA <335286@dadeschools.net>
2024-05-16 09:33:50 -0400

    Adding inbuild data annotations validation

--- Model/Address.cs
+++ Model/Address.cs
@@ -6,18 +6,19 @@ namespace BlazingPizzaNavigation.Model;
 public class Address
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "You must set a name.")]
+    [Required(ErrorMessage = "You must set a name."), MinLength(3), MaxLength(100)]
     public string Name { get; set; }
-    [Required(ErrorMessage = "You must set your address.")]
+    [Required(ErrorMessage = "You must set your address."), MinLength(5), MaxLength(100)]
     public string Line1 { get; set; }
-
+    [MaxLength(100)]
     public string Line2 { get; set; }
 
     public string City { get; set; }
-
+    [Required, MinLength(3), MaxLength(20)]
     public string Region { get; set; }
-
-    [PostalCodeAttribute]
+    [Required, RegularExpression(@"^([0-9]{5})$")]
+    //custom data annotation validator attribute.
+    //[PostalCodeAttribute]
     public string PostalCode { get; set; }
 }
 
